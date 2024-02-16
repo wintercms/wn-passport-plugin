0eea28774b399552cd1d0b54e9b62e18c9fbeb47
AIC BV <89913092+AIC-BV@users.noreply.github.com>
2024-02-16 15:16:46 +0100

    Update Plugin.php

--- Plugin.php
+++ Plugin.php
@@ -46,7 +46,7 @@ class Plugin extends PluginBase
      * Boots (configures and registers) any packages found within this plugin's packages.load configuration value
      *
      * @see https://luketowers.ca/blog/how-to-use-laravel-packages-in-october-plugins
-     * @author Luke Towers <octobercms@luketowers.ca>
+     * @author Luke Towers <info@luketowers.ca>
      */
     public function bootPackages()
     {
